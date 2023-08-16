7619631c9337611fdd9ebd0baa89918d33485495
Coderchacha <toffees9596@gmail.com>
2023-08-16 20:39:32 +0900

    TODO LIST 완료된 태스크 토글 버튼 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-interface TodoProps {
+interface item {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface todoProps {
   checked: boolean;
-  onClick: () => void;
 }
 
 export const App = () => {
-  const [isChecked, setIsChecked] = React.useState(false);
+  const [todos, setTodos] = useState<item[]>([
+    { id: 1, text: '공부하기', completed: false },
+    { id: 2, text: '장보기', completed: false },
+  ]);
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
+  const handleCheckboxChange = (id: number) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+    });
   };
 
   return (
@@ -24,28 +39,38 @@ export const App = () => {
 
         <ul>
           <TodoListTitle>ToDo List</TodoListTitle>
-          <TodoList>
-            <TodoListCheckBox
-              checked={isChecked}
-              onClick={handleCheckboxChange}
-            />
-            <TodoListLabel>운동하기</TodoListLabel>
-            <TodoListDeleteBtn>
-              <TodoListDeleteBtnSvg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                stroke-width='1.5'
-                stroke='currentColor'
-                className='w-6 h-6'
-              >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
-                />
-              </TodoListDeleteBtnSvg>
-            </TodoListDeleteBtn>
-          </TodoList>
+
+          {todos.map((todo) => {
+            return (
+              <TodoList key={todo.id}>
+                <TodoTextWrapper
+                  onClick={() => {
+                    handleCheckboxChange(todo.id);
+                  }}
+                >
+                  <TodoListCheckBox checked={todo.completed} />
+                  <TodoListLabel completed={todo.completed}>
+                    {todo.text}
+                  </TodoListLabel>
+                </TodoTextWrapper>
+                <TodoListDeleteBtn>
+                  <TodoListDeleteBtnSvg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    stroke-width='1.5'
+                    stroke='currentColor'
+                    className='w-6 h-6'
+                  >
+                    <path
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                      d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+                    />
+                  </TodoListDeleteBtnSvg>
+                </TodoListDeleteBtn>
+              </TodoList>
+            );
+          })}
         </ul>
       </Todo>
     </Background>
@@ -78,13 +103,13 @@ const TodoHeader = styled.div`
   margin-bottom: 20px;
 `;
 
-const TodoListTitle = styled.h2`
-  font-size: 15px;
+const Title = styled.h1`
+  font-size: 20px;
   margin-bottom: 10px;
 `;
 
-const Title = styled.h1`
-  font-size: 20px;
+const TodoListTitle = styled.h2`
+  font-size: 15px;
   margin-bottom: 10px;
 `;
 
@@ -116,9 +141,16 @@ const TodoList = styled.li`
   align-items: center;
   font-size: 15px;
   margin-bottom: 10px;
+  cursor: pointer;
 `;
 
-const TodoListCheckBox = styled.div<TodoProps>`
+const TodoTextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TodoListCheckBox = styled.div<todoProps>`
   width: 20px;
   height: 20px;
   border: 2px solid #ccc;
@@ -129,10 +161,11 @@ const TodoListCheckBox = styled.div<TodoProps>`
   margin-right: 8px;
 `;
 
-const TodoListLabel = styled.label`
+const TodoListLabel = styled.label<{ completed: boolean }>`
   font-size: 15px;
+  cursor: pointer;
+  ${(props) => props.completed && 'text-decoration: line-through;'};
 `;
-
 const TodoListDeleteBtn = styled.button``;
 
 const TodoListDeleteBtnSvg = styled.svg`
