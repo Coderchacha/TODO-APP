c880e30dade70034d697bbc719348218f94d8497
Coderchacha <toffees9596@gmail.com>
2023-08-17 16:17:16 +0900

    리스트 비어있을 때 alert UI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -80,8 +80,51 @@ export const App = () => {
 
         <ul>
           <TodoListTitle>ToDo List</TodoListTitle>
+          {todos.length !== 0 ? (
+            todos.map((todo) => {
+              return (
+                <TodoList key={todo.id}>
+                  <TodoTextWrapper
+                    onClick={() => {
+                      handleCheckboxChange(todo.id);
+                    }}
+                  >
+                    <TodoListCheckBox checked={todo.completed} />
+                    <TodoListLabel completed={todo.completed}>
+                      {todo.text}
+                    </TodoListLabel>
+                  </TodoTextWrapper>
+                  <TodoListDeleteBtn
+                    onClick={() => {
+                      deleteTodo(todo.id);
+                    }}
+                  >
+                    <TodoListDeleteBtnSvg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 24 24'
+                      stroke-width='1.5'
+                      stroke='currentColor'
+                      className='w-6 h-6'
+                    >
+                      <path
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+                      />
+                    </TodoListDeleteBtnSvg>
+                  </TodoListDeleteBtn>
+                </TodoList>
+              );
+            })
+          ) : (
+            <TodoEmptyAlert>새로운 GOAL을 추가해주세요.</TodoEmptyAlert>
+          )}
 
-          {todos.map((todo) => {
+          {/* { === null && (
+            <TodoEmptyAlert>새로운 GOAL을 추가해주세요.</TodoEmptyAlert>
+          )} */}
+
+          {/* {todos.map((todo) => {
             return (
               <TodoList key={todo.id}>
                 <TodoTextWrapper
@@ -115,7 +158,7 @@ export const App = () => {
                 </TodoListDeleteBtn>
               </TodoList>
             );
-          })}
+          })} */}
         </ul>
       </Todo>
     </Background>
@@ -156,6 +199,15 @@ const Title = styled.h1`
 const TodoListTitle = styled.h2`
   font-size: 15px;
   margin-bottom: 10px;
+`;
+
+const TodoEmptyAlert = styled.div`
+  font-size: 15px;
+  color: #fff;
+  background-color: #cfcbc8;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 26px 25px 23px 0px rgba(0, 0, 0, 0.1);
 `;
 
 const TodoInput = styled.input`
